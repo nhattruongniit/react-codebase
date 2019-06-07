9fca853777311c7ed5a3aa4374406ccb081cd794
Truong <ty339178@toyo-local.co.jp>
2019-06-07 14:35:48 +0700

    modify setting redux tools

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory';
-import { applyMiddleware, compose, createStore } from 'redux';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import createHistory from "history/createBrowserHistory";
+import { applyMiddleware, compose, createStore } from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-import { App, Login, Register, ForgotPassword, ResetPassword } from 'feature';
-import combinedReducers from './reducer';
-import * as serviceWorker from './serviceWorker';
+import { App, Login, Register, ForgotPassword, ResetPassword } from "feature";
+import combinedReducers from "./reducer";
+import * as serviceWorker from "./serviceWorker";
 
-import './scss/index.scss';
+import "./scss/index.scss";
 
 const history = createHistory();
 const middleware = routerMiddleware(history);
 
-const composeEnhancers = process.env.NODE_ENV === 'production'
-  ? compose
-  : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === undefined // eslint-disable-line
-    ? compose
-    : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__; // eslint-disable-line
+// const composeEnhancers = process.env.NODE_ENV === 'production'
+//   ? compose
+//   : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === undefined // eslint-disable-line
+//     ? compose
+//     : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__; // eslint-disable-line
+const composeEnhancers =
+  process.env.NODE_ENV === "development" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
 
 const store = createStore(
   connectRouter(history)(combinedReducers),
   composeEnhancers(
     applyMiddleware(routerMiddleware(history)),
     applyMiddleware(middleware),
-    applyMiddleware(thunk),
+    applyMiddleware(thunk)
     // applyMiddleware(epicMiddleware),
-  ),
+  )
 );
 
 const ReactApp = () => {
-  console.log('app re-render app');
+  console.log("app re-render app");
   return (
     <BrowserRouter>
       <Provider store={store}>
@@ -45,8 +50,16 @@ const ReactApp = () => {
           <Switch>
             <Route exact path="/:nameApp/login" component={Login} />
             <Route exact path="/:nameApp/register" component={Register} />
-            <Route exact path="/:nameApp/forgot-password" component={ForgotPassword} />
-            <Route exact path="/:nameApp/reset-password" component={ResetPassword} />
+            <Route
+              exact
+              path="/:nameApp/forgot-password"
+              component={ForgotPassword}
+            />
+            <Route
+              exact
+              path="/:nameApp/reset-password"
+              component={ResetPassword}
+            />
             <Route path="/:nameApp/" component={App} />
           </Switch>
         </MuiThemeProvider>
@@ -55,9 +68,7 @@ const ReactApp = () => {
   );
 };
 
-ReactDOM.render((
-  <ReactApp />
-), document.getElementById('root'));
+ReactDOM.render(<ReactApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
